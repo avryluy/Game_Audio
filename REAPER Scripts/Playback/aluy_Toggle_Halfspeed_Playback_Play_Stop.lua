60b5c15a2ebaeccc73d139c6eec69bd73b729cf4
Avry Luy <avryluy@mailbox.org>
2023-08-09 15:36:00 -0500

    removed unnecessary if checks

--- REAPER Scripts/Playback/aluy_Toggle_Halfspeed_Playback_Play_Stop.lua
+++ REAPER Scripts/Playback/aluy_Toggle_Halfspeed_Playback_Play_Stop.lua
@@ -3,7 +3,6 @@ local project = reaper.EnumProjects(-1)
 -- Global Action Variable
 Play_Stop_ID = 40044 --Transport: Play/Stop action
 
-
 --Check for project running
 if project then
     --Get playrate of current project
@@ -11,12 +10,7 @@ if project then
     --Get playstate of current project
     local playstate = reaper.GetPlayState()
 
-    --If paused and Playrate is 1
-    if playrate == 1 and playstate == 0 then
-        reaper.CSurf_OnPlayRateChange(.5)
-        reaper.Main_OnCommandEx(Play_Stop_ID, 0, project)
-    --If paused and Playrate is greater than but not equal to 1
-    elseif playrate > 0 and playrate ~= 1 and playstate == 0 then
+    if playstate == 0 then
         reaper.CSurf_OnPlayRateChange(.5)
         reaper.Main_OnCommandEx(Play_Stop_ID, 0, project)
     else
